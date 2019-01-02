2fabd82ebbb60506693d864aee11b882f8bccc34
Vernieri <jvmendes.ti@gmail.com>
2019-01-02 16:08:08 -0200

    Update smtpenum2.py

--- scans/smtpenum2.py
+++ scans/smtpenum2.py
@@ -7,6 +7,7 @@ file = open('YOUR_LIST_HERE.txt')
 for linha in file.readlines():
   s = socket.socket(socket.AF_INET, SOCK_STREAM)
   s.connect((sys.argv[1], 25))
+  
   resp = s.recv(1024)
   s.send("VRFY"+linha)
   resp = s.recv(1024)
