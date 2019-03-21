3faf2414ddf1384855f3d5dc79a162724a1e43a9
Vernieri <jvmendes.ti@gmail.com>
2019-03-21 08:42:38 -0300

    Update nrepiter_pattern.py

--- pyttern-matching/nrepiter_pattern.py
+++ pyttern-matching/nrepiter_pattern.py
@@ -4,7 +4,28 @@ def starter():
 	value = raw_input('Quantidade: ')
 	loop(int(value))
 
+def loop(value):
+	lista = []
+	i = 1
+	while i <= value:
+		
+		hexa(i, lista)
+		s = ''.join(lista)
 
+		
+		i += 1
+
+	
+	print(s)
+	f = open('pattern.txt', 'w')
+	f.write(s)
+	f.close
+	op = raw_input('Deseja continuar ou deseja sair<s/n>? ') #I might create a method just for it...
+	if op=='s':
+		number = raw_input("Digite o valor que deseja encontrar: ")
+		finder(s, number)
+	else:
+		exit()
 
 
 starter()		
