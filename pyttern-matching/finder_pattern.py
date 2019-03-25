6bafb734077781166802530287638aef37c671f0
Vernieri <jvmendes.ti@gmail.com>
2019-03-25 10:51:03 -0300

    Update finder_pattern.py

--- pyttern-matching/finder_pattern.py
+++ pyttern-matching/finder_pattern.py
@@ -6,3 +6,17 @@ def starter():
 	number = raw_input("Passa o Pattern: ")
 	finder(s, number)
 	f.close()
+	
+def find_str(s, char):
+    index = 0
+
+    if char in s:
+        c = char[0]
+        for ch in s:
+            if ch == c:
+                if s[index:index+len(char)] == char:
+                    return index
+
+            index += 1
+
+    return -1
